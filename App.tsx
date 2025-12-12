6afefac61108062e6004aa58a7069ef5161868cf
Jayaseelan123 <jayaseelanravi07@gmail.com>
2025-12-12 12:11:46 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import GallerySection from './components/GallerySection';
-import StatsSection from './components/StatsSection';
-import VideoSection from './components/VideoSection';
-import AboutSection from './components/AboutSection';
-import PortfolioSection from './components/portfolio';
-import ContactSection from './components/ContactSection';
-import ServicesSection from './components/ServicesSection';
-import Footer from './components/Footer';
+import React from "react";
 
-import ClickEffect from "./components/ClickEffect";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import ServicesSection from "./components/servicessection";
+import PortfolioSection from "./components/PortfolioSection";
+import GallerySection from "./components/GallerySection";
+import StatsSection from "./components/statssection"
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
-
+import ClickEffect from "./components/clickeffect";
 
 const App: React.FC = () => {
   return (
     <div className="font-sans bg-gradient-to-b from-white via-gray-100 to-gray-900 text-gray-800 relative">
-      <ClickEffect /> 
+      <ClickEffect />
+
       <Header />
+
       <main>
         <HeroSection />
         <AboutSection />
         <ServicesSection />
         <PortfolioSection />
-        {/* <VideoSection /> */}
-       
         <GallerySection />
-        {/* <VideoSection /> */}
         <StatsSection />
         <ContactSection />
       </main>
+
       <Footer />
-      {/* <ScrollToTopButton /> */}
     </div>
   );
 };
